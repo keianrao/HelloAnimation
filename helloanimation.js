fd8779d058fc647536767b373606d6faab80496d
Keian Rao <keian.rao@gmail.com>
2020-09-28 22:21:36 -0400

    Added first navigation buttons, including their animation.

--- helloanimation.js
+++ helloanimation.js
@@ -1,6 +1,44 @@
 
 const data = { };
 
+const navButtons = {
+	"Contact": undefined,
+	"Projects": undefined
+}
+/*
+* Intentionally using undefined. We're not defining them yet,
+* but I'd just like to list the identifiers here, for reference.
+*/
+	
+const pages = {
+	"Contact": {
+		name: undefined
+	},
+	"Projects": {
+		java: {
+			containerElem: undefined,
+			headerElem: {
+				iconElement: undefined,
+				headingElement: undefined,
+				iconURL: undefined 
+				// I think we should define iconURL here and now. As a constant.
+			}
+		}
+	}
+}
+
+
+
+//  \\  //  \\  //  \\  //  \\  //  \\  //  \\
+
+function setPage(pageName) {
+	// Change look of the buttons?
+}
+
+
+
+//  \\  //  \\  //  \\  //  \\  //  \\  //  \\
+
 function extractDataFromOriginalHtmlElements() {
 	// Extract from 'contact' section.
 	data.contact = {};	
@@ -49,14 +87,52 @@ function removeOriginalHtmlElements() {
 	// we have nothing to preserve.
 }
 
-function renderJavaScriptPage() {
+function initialiseNewHtmlElements() {
+	// The data needs to be ready when this is called.
+	
+	// Initialise navigation buttons.
+	const NAVBUTTON_CLASS = "js-navbutton";	
+	navButtons["Contact"] = document.createElement("button");
+	navButtons["Contact"].setAttribute("onclick", "setPage('Contact')");
+	navButtons["Contact"].setAttribute("class", NAVBUTTON_CLASS);
+	navButtons["Contact"].setAttribute("id", NAVBUTTON_CLASS + "-contact");
+	navButtons["Contact"].innerText = "Contact";
+	document.body.append(navButtons["Contact"]);
+	navButtons["Projects"] = document.createElement("button");
+	navButtons["Projects"].setAttribute("onclick", "setPage('Projects')");
+	navButtons["Projects"].setAttribute("class", NAVBUTTON_CLASS);
+	navButtons["Projects"].setAttribute("id", NAVBUTTON_CLASS + "-projects");	
+	navButtons["Projects"].innerText = "Projects";
+	document.body.append(navButtons["Projects"]);
+	// Initialisation-wise there aren't really any differences between them.
+	// Maybe we should break this into a loop over the nav buttons.
+}
 
+function moveNavButtonsIntoPlace() {
+	const NAVBUTTON_FINAL_TOP = "5%";
+	setTimeout(function () {
+		var css = navButtons["Contact"].style;		
+		css.top = NAVBUTTON_FINAL_TOP;
+		css.opacity = 100;
+	}, 500);
+	setTimeout(function () {
+		var css = navButtons["Projects"].style;
+		css.top = NAVBUTTON_FINAL_TOP;
+		css.opacity = 100;
+	}, 750);
+	// Should we block here?
+}
+
+function renderJavaScriptPage() {
+	initialiseNewHtmlElements();
+	moveNavButtonsIntoPlace();
+	setPage("Contact");
 }
 
 function transitionToJS() {
 	extractDataFromOriginalHtmlElements();
-	// removeOriginalHtmlElements();
-	// renderJavaScriptPage();
+	removeOriginalHtmlElements();
+	renderJavaScriptPage();
 }
 
 transitionToJS();
